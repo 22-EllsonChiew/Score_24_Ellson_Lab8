7329edd4003cf5918e31c1a36398d0ae8ce57dc0
Itzuzume <ellson637@gmail.com>
2020-11-05 14:53:20 +0800

    Score_23_Ellson_Lab8

---
+++ 24_Ellson_Lab8/Assets/TextScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+
+    public GameObject scoreText;
+    private int score;
+    public Text ScoreText;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = "SpaceBar: ";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = "SpaceBar: " + score;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            score = score + 1;
+           
+        }
+       
+    }
+    
+   
+}
